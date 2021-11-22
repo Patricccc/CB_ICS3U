d146accd124a8936b5eb091dca5e14d85875fefc
Patricccc <pattytroller@gmail.com>
2021-11-22 13:27:43 -0500

    Add files via upload

---
+++ Unit 5: Strings and Arrays/MaxMultiDimArray.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class MaxMultiDimArray {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int r = scan.nextInt();
+        int in;
+        int[][] arr = new int[r][];
+        for(int i = 0;i < r;i++){
+            in = scan.nextInt();
+            arr[i] = new int[in];
+            for(int j = 0;j < arr[i].length;j++){
+                arr[i][j] = scan.nextInt();
+            }
+        }
+        System.out.println(max(arr));
+        scan.close();
+    }
+    public static int max(int[][] arr){
+        int max = Integer.MIN_VALUE;
+        for(int i = 0;i < arr.length;i++){
+            for(int j = 0;j < arr[i].length;j++){
+                if(arr[i][j] > max) max = arr[i][j];
+            }
+        }
+        return max;
+    }
+}
